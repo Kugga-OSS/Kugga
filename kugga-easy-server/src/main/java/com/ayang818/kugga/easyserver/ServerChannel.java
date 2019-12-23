c267782b84a409671c49e1dc50419ca5f3653e34
杨丰畅 <ayang818@qq.com>
2019-12-23 16:57:24 +0800

    update maven dependencies

--- kugga-easy-server/src/main/java/com/ayang818/kugga/easyserver/ServerChannel.java
+++ kugga-easy-server/src/main/java/com/ayang818/kugga/easyserver/ServerChannel.java
@@ -6,5 +6,4 @@ package com.ayang818.kugga.easyserver;
  * @date 2019/12/18 20:44
  **/
 public class ServerChannel {
-
 }
---
+++ kugga-starter/src/main/java/com/ayang818/kugga/starter/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.ayang818.kugga.starter.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.Servlet;
+
+/**
+ * @author 杨丰畅
+ * @description TODO
+ * @date 2019/12/23 16:52
+ **/
+@RestController
+public class HelloController {
+    Servlet
+}
