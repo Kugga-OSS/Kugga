a18ec7602f18a58f84344cf6328cf63fbe836e86
ayang818 <ayang818@qq.com>
2020-01-15 15:37:01 +0800

    修复kugga-starter中plugin里的dependency.version引用父pom的版本号,导致CI的自动构建失败的BUG(可能是CI中maven版本的问题)

--- kugga-netty/src/main/java/com/ayang818/kugga/netty/websocket/ChatHandler.java
+++ kugga-netty/src/main/java/com/ayang818/kugga/netty/websocket/ChatHandler.java
@@ -10,6 +10,35 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
+ *                               ——————————————————————————
+ *                               已经登录，每次进入主界面，且上
+ *                              一个连接过期了，重新创建channel。
+ *                               ——————————————————————————
+ *                                          |
+ *                                          |
+ *                                          V
+ *                               ———————————————————————————
+ *                               创建维护用户Id与ChannelShortId
+ *                               的ConcurrentHashMap映射。用于
+ *                               在线用户的信息维护。
+ *                               ————————————————————————————
+ *                                          |
+ *                                          |
+ *                                          V
+ *                               ————————————————————————————
+ *                               每次Handler收到传入的信息时(
+ *                                  传入的信息有几种类别
+ *                                  1. 消息
+ *                                  2. 心跳包
+ *                                  3. 新的Channel连接
+ *                               )
+ *                               根据消息类别的不同做出不同的行为。
+ *                               1. 若是消息，进行消息的存储
+ *                               2. 若是新的Channel连接，拉取对应用户的未读消息。
+ *                               3. 若是心跳包，不做该Handler做处理。
+ *                               ————————————————————————————
+ *
+ *
  * @author 杨丰畅
  * @description TODO
  * @date 2020/1/13 12:01
