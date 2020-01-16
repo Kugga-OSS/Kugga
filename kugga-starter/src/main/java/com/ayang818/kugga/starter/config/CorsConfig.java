639807d4102aac7696cbb67202881abb9035cc6f
ayang818 <ayang818@qq.com>
2020-01-16 14:54:42 +0800

    添加cors跨域

---
+++ kugga-starter/src/main/java/com/ayang818/kugga/starter/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.ayang818.kugga.starter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author 杨丰畅
+ * @description TODO
+ * @date 2020/1/16 12:51
+ **/
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
